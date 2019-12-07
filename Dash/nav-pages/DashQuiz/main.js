09176c14c54f94998d039902f9ae27e19419ea75
Colin Davis <colindavis5@gmail.com>
2019-12-07 12:59:51 -0500

    finished js for quiz mostly, added anti inspect

--- Dash/nav-pages/DashQuiz/main.js
+++ Dash/nav-pages/DashQuiz/main.js
@@ -15,11 +15,10 @@ var currentQuestion = " "
 //document.onload(loadHighScore())
 
 class question{constructor(questionNum, question, answerA, answerB, answerC, answerD, correctAnswer) {
-    //this.questionNum = questionNum 
     this.questionIndex = questionNum //question "index" or its place in the possibleQuestions array
     this.question = question
     this.answers = [ //store all possible answers in array
-        answerA, answerB, answerC, answerD//at some point you've seen answer so many times it no longer looks like a word
+        answerA, answerB, answerC, answerD//at some point you've seen answer/question so many times it no longer looks like a word
     ] //you start to second guess your spelling
     this.rightAnswer = correctAnswer //slowly seeping into madness
 }}
@@ -34,6 +33,7 @@ let question7 = new question(6, "What colored role is most popular(has the most 
 let question8 = new question(7, "How many bots does Dash Net have?", "12", "1", "2", "10", "10")
 let question9 = new question(8, "What channel do you need the pillow role to view?", "#Lobby", "#Rulebook", "#Cozy", "#Cubic-Wonderland", "#Cozy")
 let question10 = new question(9, "What year was Cookie Clicker released?", "2005", "2013", "2019", "2014", "2013")
+//let question11 = new question(10, "answer is A", "A", "B", "C", "D", "A")
 
 let possibleQuestions = [
     question1, 
@@ -46,6 +46,7 @@ let possibleQuestions = [
     question8, 
     question9, 
     question10
+    //question11
 ]
 
 function newGame() {
@@ -69,7 +70,9 @@ function loadHighScore() {
     if(highScore == undefined) {
         return
     } else {
-        document.getElementById("highScoreDisplay").innerHTML = window.localStorage.getItem('score')
+        document.getElementById("highScore").innerHTML = window.localStorage.getItem('score')
+        document.getElementById("highScoreDisplay").style = "width: auto; float: right; visibility: visible;"
+        document.getElementById("highScore").style = "visibility: visible;"
     }
 }
 
@@ -82,7 +85,7 @@ function isCheck(e) {
         //delete possibleQuestions[currentQuestion.questionIndex]
         questionIndex.push(currentQuestion.questionIndex)
         //possibleQuestions.pop()
-        if(questionIndex.length===10) {
+        if(questionIndex.length===possibleQuestions.length) {
             gameOver()
         } else {
             newQuestion()
@@ -90,7 +93,7 @@ function isCheck(e) {
     } else {
         //score--
         alert("That is incorrect! The right answer was " + currentQuestion.rightAnswer)
-        if(possibleQuestions.length==0) {
+        if(questionIndex.length==possibleQuestions.length) {
             gameOver()
         } else {
             newQuestion()
@@ -114,18 +117,19 @@ function generateNewQuestionContent() {
 function newQuestion() {
     var i = Math.floor(Math.random()*possibleQuestions.length)
     currentQuestion = possibleQuestions[i]
-    //myLoop:
+    newQuestionForLoop:
     for(i=0;i<possibleQuestions.length;i++) {
         if (currentQuestion.questionIndex === questionIndex[i]) {
             newQuestion() //question has already been used
             clearTimeout(i)
-            //break myLoop
+            break newQuestionForLoop
         }else if(currentQuestion.questionIndex !== questionIndex[i]) {
             generateNewQuestionContent() //we have a new question
         } else if(i==possibleQuestions.length) {
             gameOver() //the other function didn't do its job so we have to pick up the slack here
         }
     }
+    return
 }
 
 function gameOver() {
@@ -138,7 +142,12 @@ function gameOver() {
         document.getElementById("highScoreDisplay").innerHTML = highScore
         document.getElementById("highScoreDisplay").style = "visibility: visible"
     }
-    
+
+    //wipe answers, we just want score and high score showing
+    document.getElementById("answerA").style = "visibility: hidden"
+    document.getElementById("answerB").style = "visibility: hidden"
+    document.getElementById("answerC").style = "visibility: hidden"
+    document.getElementById("answerD").style = "visibility: hidden"
 
     //alert("You have finished this quiz! Your final score was " + score)
 }
