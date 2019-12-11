7e398c7ef652c04392ac16140bd784a9153b25aa
Colin Davis <colindavis5@gmail.com>
2019-12-10 19:53:34 -0500

    started on acheivments

---
+++ Dash/DashIdleGame/acheivments.js
@@ -0,0 +1,10 @@
+class acheivment{constructor(name, neededBuilding, neededBuildingAmount){
+    this.name = name
+
+    this.building = neededBuilding
+    this.buildingAmount = neededBuildingAmount
+
+    this.earn = ()=>{
+        
+    }
+}}
