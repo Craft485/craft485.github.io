5feb14f6eaf65bbd2bd14d8b379059ac36598133
Colin Davis <colindavis5@gmail.com>
2019-12-15 16:43:31 -0500

    temp removed showDescription

--- Dash/DashIdleGame/acheivments.js
+++ Dash/DashIdleGame/acheivments.js
@@ -12,19 +12,49 @@ class acheivment{constructor(name, neededBuilding, neededBuildingAmount, display
     this.earn = ()=>{
         if(this.building.amountOwned>=this.buildingAmount && this.owned === false) {
             let iframe = document.getElementById("achievmentIFrame")
-            let iframeDoc = iframe.contentDocument || iframe.contentWindow.document
+            let iframeDoc = iframe.contentDocument || iframe.contentWindow.document //this wont work locally as it is seen as xss
             let chev = iframeDoc.getElementById(this.name)
             chev.innerHTML = this.displayName
 
             this.owned = true
             
+            //this.showDescription()
         }
     }
     
+    /*this.notify = ()=>{
+        
+    }*/
+
     this.showDescription = ()=>{
-        alert(this.description)
-        //readd onclick event listener(anon arrow function)
-        document.getElementById(this.name).onclick = ()=>{this.showDescription()}
+        if(this.owned===true) {
+            let newPopUp = document.createElement("div")
+
+            let newPopUpName = document.createElement("p")
+            newPopUpName.innerHTML = this.name
+            //newPopUpName.style
+            let newPopUpDesc = document.createElement("p")
+            newPopUpDesc.innerHTML = this.description
+            newPopUp.appendChild(newPopUpName)
+            newPopUp.appendChild(document.createElement("br"))
+            newPopUp.appendChild(newPopUpDesc)
+            //newPopUp.innerHTML = this.description
+
+            newPopUp.style = "margin-left: 50%;"
+
+            let notifBar = document.getElementById("notificationBarWrapper")
+
+            notifBar.appendChild(newPopUp)
+
+            setTimeout(_=>{
+                notifBar.removeChild(newPopUp)
+            }, 3000)
+
+            // alert(this.description)
+
+            //readd onclick event listener(anon arrow function)
+            document.getElementById(this.name).onclick = ()=>{this.showDescription()}
+        }
     }
 }}
 
--- Dash/DashIdleGame/noControl.js
+++ Dash/DashIdleGame/noControl.js
@@ -1,5 +1,30 @@
-$(document).keydown(function(e){
-    if(e.which === 123 || e.which === 17){
-       return false;
+/*document.addEventListener(onkeydown, (e)=>{
+    if(e.ctrlKey && e.shiftKey) {
+        e.preventDefault()
+        return false
     }
-})
+})*/
+document.addEventListener('keydown', function() {
+    if (event.keyCode == 123) {
+      //alert("This function has been disabled to prevent you from stealing my code!");
+      return false
+    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+      //alert("This function has been disabled to prevent you from stealing my code!")
+      return false;
+    } else if (event.ctrlKey && event.keyCode == 85) {
+      //alert("This function has been disabled to prevent you from stealing my code!")
+      return false;
+    }
+  }, false);
+  
+  if (document.addEventListener) {
+    document.addEventListener('contextmenu', function(e) {
+      //alert("This function has been disabled to prevent you from stealing my code!")
+      e.preventDefault()
+    }, false)
+  } else {
+    document.attachEvent('oncontextmenu', function() {
+      // alert("This function has been disabled to prevent you from stealing my code!")
+      window.event.returnValue = false
+    });
+  }
